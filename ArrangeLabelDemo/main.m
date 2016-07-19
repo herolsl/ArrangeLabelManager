e548c87381e3c900e8b3d225eda6b0f5c7147a7b
Sven Liu <Sven@sven-imac.local>
2016-07-19 09:55:52 +0800

    Initial Commit

---
+++ ArrangeLabelDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ArrangeLabelDemo
+//
+//  Created by Sven Liu on 16/7/19.
+//  Copyright © 2016年 Sven Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
